afb1e461ee246e8ac6a912ddb45b45cf428859fc
s4055832 <s4055832@student.rmit.edu.au>
2025-05-06 19:44:51 +1000

    a

--- Assignment2/study.js
+++ Assignment2/study.js
@@ -6,11 +6,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const switchBtn = document.getElementById("switchBtn");
 
   // Go back to homepage
+  // Give users the opportunity to change their plans
   backBtn.addEventListener("click", () => {
     window.location.href = "index.html";
   });
 
   // Load pomodoro count
+  // Let the Pomodoro timer be calculated by multiplying the previously set time by 25 minutes.
   const storedCount = parseInt(localStorage.getItem("pomodoroCount"), 10);
   const sessions = isNaN(storedCount) || storedCount < 1 ? 1 : storedCount;
   let totalSeconds = sessions * 25 * 60;
@@ -26,10 +28,11 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   );
 
-  // Alert sound
+  // Alert sound, Pomodoro countdown reminder
   const alarmSound = new Audio("ding.mp3");
 
   // Video sources & loop
+  // Because the video is relatively short, in order to make the video length fit the Pomodoro timer, it is played in a loop
   const sources = ["normal.mp4", "normal1.mp4", "normal2.mp4"];
   let current = 0;
   video.loop = true;
@@ -38,7 +41,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let timerInterval = null;
   let isPlaying = false;
 
-  // Format time as MM:SS
+  // Format time as MM:SS，make the Pomodoro display closer to reality
   function formatTime(sec) {
     const m = String(Math.floor(sec / 60)).padStart(2, "0");
     const s = String(sec % 60).padStart(2, "0");
@@ -51,6 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
   updateDisplay();
 
   // Switch video source
+  // A single video may not meet the needs of users, so multiple comparison videos are added for easy selection
   switchBtn.addEventListener("click", () => {
     current = (current + 1) % sources.length;
     video.src = sources[current];
@@ -59,6 +63,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   // Toggle play/pause
+  // The play and pause buttons control the video, audio and Pomodoro timer in a unified way
   playBtn.addEventListener("click", () => {
     if (!isPlaying) {
       // Start playback
